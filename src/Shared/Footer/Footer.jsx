6bad38034c4712a6671a4e33b268068dd5178b4f
MD Naem <mmnayeem408@gmail.com>
2023-06-12 23:02:41 +0600

    footer copyrit add

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -7,7 +7,13 @@ const Footer = () => {
                 <img src="" alt="" />
                 <p>Skill Hub Industries Ltd.<br />Providing reliable tech since 1992</p>
             </div>
-           
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
             <div>
                 <span className="footer-title">Company</span>
                 <a className="link link-hover">About us</a>
